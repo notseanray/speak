e35f577ecc6cfa08495317a1924cef3e8efb29fc
Blyxyas <blyxyas@gmail.com>
2022-01-09 00:16:09 +0100

    🙌 Train & Run remade to macros & Divided + Readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# README is WIP
+<h1 align=center>Speak</h1>
+**Speak** is a simple, easy to use Natural Language Processor (NLP) written in Rust.
+
+<h1 align=center>Why use Speak?</h1>
+Speak uses a custom engine, and to setup you just need two functions and data.
+Speak doesn't use other crates, and itself is very lightweight, so it wont slow down your program. But while being lightweight Speak also is very powerful, with various default functions, and customization available.
+
+<h1 align=center>Features</h1>
+
+* The Rust file is very small, less than 4 KB, while being easy to understand, so you can even customize the engine for your purposes. (I recommend trying to customize the main function before modifying the engine for yourself.)
+
+* A lot of customization and dynamic range, there is practically no limit, even with the default settings. (Unless you want to make a +131k index map)
+
+* Custom map types, for optimization purposes. (Named Data and TranslatedData)
+* Fully 100% Open Source.
+
+* Multithreaded (Soon)
+* Easy setup
+
+<h1 align=center>Setup</h1>
+Currently Speak is not published, and is in Beta, please don't use it.
+
+<div align=center>
+
+[![Stargazers repo roster for @blyxyas/speak](https://reporoster.com/stars/blyxyas/speak)](https://github.com/blyxyas/speak/stargazers)
+
+[![Forkers repo roster for @blyxyas/speak](https://reporoster.com/forks/blyxyas/speak)](https://github.com/blyxyas/speak/network/members)
+
+</div>
--- src/lib.rs
+++ src/lib.rs
@@ -68,37 +68,49 @@ pub mod data {
     }
 }
 
+#[allow(unused_macros)]
+macro_rules! train {
+    (rawdata: data::Data, while_not: ident) => {
+        train!($data::Data, $while_not());
+    };
 
-#[allow(unused_variables)]
-pub fn run(rawinput: &str, rawdata: data::Data, no_similarity_found: impl Fn()){
-    let data: data::TranslatedData = rawdata.translate();
-    let mut temp: Vec<(f32, usize)> = Vec::new();
-    for (i, phrase) in data.keys.iter().enumerate() { // A phrase is each list of numbers
-        for (j, word) in phrase.iter().enumerate() { // each number
-            for bphrase in data.values.iter() {
-                for (z, bword) in bphrase.iter().enumerate() {
-                    let tmp = (*word / *bword - 1.0).abs();
-                    if tmp < 0.2 {
-                        temp.push((tmp * ((2 + j + z) as f32), i));
-                    };
+    (rawdata: data::Data, while_not: block) => {
+        let data: data::TranslatedData = rawdata.translate();
+        let mut temp: Vec<f32> = Vec::new();
+        for phrase in data.keys.iter() { // A phrase is each list of numbers
+            for (j, word) in phrase.iter().enumerate() { // each number
+                for bphrase in data.values.iter() {
+                    for (z, bword) in bphrase.iter().enumerate() {
+                        let tmp = (*word / *bword - 1.0).abs();
+                        if tmp < 0.2 {
+                            temp.push(tmp * ((2 + j + z) as f32));
+                        };
+                    }; //por cada frase -> cada palabra en esa frase -> cada frase b -> cada palabra en esa frase b
                 };
             };
         };
     };
-    // Usually we would sort the temp vector here, but we're going to use the position of the elements in the vector to calc ulate the origin. 
+}
 
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! run {
+    (rawinput: expr, not_found: ident) => {
+        let mut sum: f32 = 0.0;
+        let mut input: Vec<f32> = Vec::new();
 
-
-    // Translate Input to numbers (92 - 101)
-    let mut sum: f32 = 0.0;
-    let mut input: Vec<f32> = Vec::new();
-
-    for word in rawinput.split_whitespace() {
-        for character in word.chars() {
-            sum += (7 * character as u32) as f32;
+        for word in $rawinput.split_whitespace() {
+            for character in word.chars() {
+                sum += (7 * character as u32) as f32;
+            };
+            input.push(sum);
+            sum = 0.0;
         };
-        input.push(sum);
-        sum = 0.0;
-    };
-    // WORKING ON HERE
+
+        // The remaining run! macro goes here (Under development)
+
+        if input.len() == 0 {
+            $not_found();
+        }
+    }
 }
