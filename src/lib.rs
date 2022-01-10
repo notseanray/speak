3517099063f1eac690ba433bb3f414da11e8cb88
Blyxyas <blyxyas@gmail.com>
2022-01-10 18:41:46 +0100

    👌Run(): Almost there!

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 pub mod data {
     pub struct Data {
         pub keys: Vec<String>,
@@ -44,9 +43,18 @@ pub mod data {
         pub fn keys(&self) -> Vec<String> {
             return self.keys.clone();
         }
+
         pub fn values(&self) -> Vec<String> {
             return self.values.clone();
         }
+
+        pub fn clone(&self) -> Data {
+            Data {
+                keys: self.keys.clone(),
+                values: self.values.clone(),
+            }
+        }
+
         pub fn translate(self: Data) -> TranslatedData {
             let mut keys: Vec<Vec<f32>> = Vec::new();
             let mut values: Vec<Vec<f32>> = Vec::new();
@@ -63,43 +71,39 @@ pub mod data {
     }
 }
 
+#[allow(dead_code)]
 fn difference(a: f32, other: f32) -> f32 {
     return (a / other - 1.0).abs();
 }
 
-#[macro_export]
-macro_rules! train {
-    (rawdata: data::Data, while_not: ident) => {
-        train!($data::Data, $while_not());
-    };
-
-    (rawdata: data::Data, while_not: block) => {
-        let data: data::TranslatedData = rawdata.translate();
-        let mut trained: Vec<f32> = Vec::new();
-        for phrase in data.keys.iter() { // A phrase is each list of numbers
-            for (j, word) in phrase.iter().enumerate() { // each number
-                for bphrase in data.values.iter() {
-                    for (z, bword) in bphrase.iter().enumerate() {
-                        let tmp = (*word / *bword - 1.0).abs();
-                        if tmp < 0.2 {
-                            trained.push(tmp * ((2 + j + z) as f32));
-                        };
-                    }; //por cada frase -> cada palabra en esa frase -> cada frase b -> cada palabra en esa frase b
+pub fn train(rawdata: data::Data) -> (Vec<Vec<f32>>, Vec<f32>) {
+    let data: data::TranslatedData = rawdata.translate();
+    let mut trained: Vec<f32> = Vec::new();
+    for phrase in data.keys.iter() {
+        // A phrase is each list of numbers
+        for (j, word) in phrase.iter().enumerate() {
+            // each number
+            for bphrase in data.values.iter() {
+                for (z, bword) in bphrase.iter().enumerate() {
+                    let tmp = (*word / *bword - 1.0).abs();
+                    if tmp < 0.2 {
+                        trained.push(tmp * ((2 + j + z) as f32));
+                    };
                 };
             };
         };
     };
-}
+    return (data.keys, trained);
+    }
 
-#[macro_export]
-macro_rules! run {
-    (rawinput: expr, not_found: block) => {{
+pub fn run(rawinput: String, trained: Vec<f32>, keys: Vec<Vec<f32>>, values: Vec<String>, callback: impl Fn()) {
         let mut sum: f32 = 0.0;
         let mut input: Vec<f32> = Vec::new();
+        let mut returnal: Vec<Option<String>> = Vec::new();
 
         // Translate input
 
-        for word in $rawinput.split_whitespace() {
+        for word in rawinput.split_whitespace() {
             for character in word.chars() {
                 sum += (7 * character as u32) as f32;
             }
@@ -109,16 +113,17 @@ macro_rules! run {
 
         for (i, number) in input.iter().enumerate() {
             for (j, bnumber) in trained.iter().enumerate() {
-                for (z, ext) in data.keys.iter().enumerate() {
-                    if difference((number / ext) * (2 + i + z), bnumber) < 0.1 {
-                        returnal.push()
-                    };
+                for list in keys.iter().enumerate() {
+                    for (z, ext) in list.1.iter().enumerate() {
+                        if difference((number / ext) * (2 + i + z) as f32, *bnumber) < 0.1 {
+                            returnal.push(values[j].split(' ').map(str::to_string).nth(z));
+                        };
+                    }
                 }
             }
         }
 
         if input.len() == 0 {
-            $not_found;
+            callback();
         };
-    };};
-}
+    }
