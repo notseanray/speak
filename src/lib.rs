58cc6580d1a7b9410e0354aeadfda8f49d457394
Blyxyas <blyxyas@gmail.com>
2022-01-11 23:10:33 +0100

    Added some 🎞️ functions and minor stylistic changes

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,35 @@
 #![allow(non_camel_case_types)]
 #![allow(unused_variables)]
 
-pub mod mapping {
-    pub struct map<K, V> {
-        entries: Vec<(K, V)>
+pub struct map<K, V> {
+    entries: Vec<(K, V)>
+}
+
+impl<K, V> map<K, V> { // Implements for maps of all types
+    pub fn new() -> map<K, V> {
+        map {
+            entries: Vec::new()
+        }
     }
 
-    impl<K, V> map<K, V> { // Implements for maps of all types
-        pub fn new() -> map<K, V> {
-            map {
-                entries: Vec::new()
-            }
+    pub fn keys(&self) -> Vec<&K>{
+        let mut x: Vec<&K> = Vec::new();
+        for (key, _) in &self.entries {
+            x.push(&key);
         }
+        x
+    }
 
-        pub fn keys(&self) -> Vec<&K>{
-            let mut x: Vec<&K> = Vec::new();
-            for (key, _) in &self.entries {
-                x.push(&key);
-            }
-            x
+    pub fn values(&self) -> Vec<&V>{
+        let mut x: Vec<&V> = Vec::new();
+        for (_, value) in &self.entries {
+            x.push(&value)
         }
+        x
+    }
+}
 
-        pub fn values(&self) -> Vec<&V>{
-            let mut x: Vec<&V> = Vec::new();
-            for (_, value) in &self.entries {
-                x.push(&value)
-            }
-            x
+
         }
     }
 }
