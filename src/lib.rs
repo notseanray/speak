7e745406f3b03ac61331b0cea6d2fc6ccc741f11
Blyxyas <blyxyas@gmail.com>
2022-01-12 18:47:28 +0100

    🦐 Minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -14,18 +14,18 @@ impl<K, V> map<K, V> { // Implements for maps of all types
         }
     }
 
-    pub fn keys(&self) -> Vec<&K>{
-        let mut x: Vec<&K> = Vec::new();
-        for (key, _) in &self.entries {
-            x.push(&key);
+    pub fn keys(&self) -> Vec<K>{
+        let mut x: Vec<K> = Vec::new();
+        for (key, _) in self.entries {
+            x.push(key);
         }
         x
     }
 
-    pub fn values(&self) -> Vec<&V>{
-        let mut x: Vec<&V> = Vec::new();
-        for (_, value) in &self.entries {
-            x.push(&value)
+    pub fn values(&self) -> Vec<V>{
+        let mut x: Vec<V> = Vec::new();
+        for (_, value) in self.entries {
+            x.push(value)
         }
         x
     }
@@ -33,12 +33,8 @@ impl<K, V> map<K, V> { // Implements for maps of all types
 
 pub fn train<K, V>(map: map<K, V>) {
     // Deconstructing map into his two arrays
-    let keys: Vec<&K> = map.keys();
-    let values: Vec<&V> = map.values();
+    let keys: Vec<K> = map.keys();
+    let values: Vec<V> = map.values();
 
     // Now, let's create a mega array.
-
-    for key in keys.iter() {
-        
-    }
 }
