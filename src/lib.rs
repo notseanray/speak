436b8937f90615d11808103f861b80c7ea4337c6
Alex <blyxyas@gmail.com>
2022-01-12 10:38:33 +0100

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -28,22 +28,27 @@ impl<K, V> map<K, V> { // Implements for maps of all types
         }
         x
     }
-
-    macro_rules! deconstruct {
-        ( $($v: expr, $nickname: expr), *) => {
-            {
-                $(
-                    let mut $nickname = Vec::new()
-                    for each in v.iter() {
-                        $nickname.push(each)
-                    };
-                );
-            };
+}
+    
+macro_rules! deconstruct {
+    ( $($v: expr, $nickname: expr), *) => {
+        {
+            $(
+                let mut $nickname = Vec::new()
+                for each in v.iter() {
+                    $nickname.push(each)
+                };
+            );
         };
     };
-}
+};
 
 pub fn train(map: map<K, V>) {
-    // Deconstruct map into his two arrays
-    deconstruct!(K, keys, V, values)
+    // Deconstructing map into his two arrays
+    map::deconstruct!(K, keys, V, values)
+    // Now, let's create a mega array.
+
+    for key in keys.iter() {
+        
+    }
 }
