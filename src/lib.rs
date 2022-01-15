bfc25853e32d5b69cb75a5bf89cc7d7421e4b56c
Blyxyas <blyxyas@gmail.com>
2022-01-15 12:46:58 +0100

    [Add] Deconstructed Struct + ...
    [Add] A little bit of the main loop
    
    [Add] Display Maps with 'display(map)' (just for displayable types)
    
    [Add] 'Reconstruct' function for 'Deconstructed' struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,35 @@
+/*
+ Speak crate made by Alex G. C. aka Blyxyas. Visit github.com/blyxyas/speak-rust for more information.
+
+This crate allows you to talk with your machine. 0 dependencies neededt. (That's the goal).
+
+TODO(s):
+- Check if this works in Linux, MacOS and old Windows.
+- Program the algorithm.
+- Create the documentation.
+
+- Complete the Rust version.
+- Create the Python version.
+- Create the WebAssembly version.
+- Create the C++ version.
+
+I think 4 libs will be enough.
+
+- Create some standarized tests.
+- Learn how to use Docker.
+- Fail the tests.
+- Cry. ✅
+
+*/
+
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(unused_variables)]
 #![allow(unused_macros)]
 
 pub mod mapping {
-    pub struct map<K, V> { // This is basically just a wrapper for the Deconstructed map.
-
+    use std::fmt::Display;
+    pub struct map<K, V> { // This is just a wrapper for the Deconstructed map.
         pub entries: Vec<(K, V)>,
     }
 
@@ -44,6 +68,30 @@ pub mod mapping {
             Deconstructed { keys, values }
         }
     }
+
+    impl<K, V> Deconstructed<K, V> {
+        pub(crate) fn reconstruct(&self) -> map<K, V> {
+            let mut entries = Vec::new();
+
+            for i in 0..(self.keys.len()) {
+                entries.push((self.keys[i], self.values[i]));
+            }
+
+            return map {
+                entries
+            };
+        }
+    }
+
+    pub(crate) fn display<T: Display>(map: map<T, T>) {
+        let mut string = String::new();
+
+        for (key, value) in map.entries.iter() {
+            string.push_str(&format!("{} => {}\n", key, value));
+        };
+
+        println!("{}", string);
+    }
 }
 
 fn contains(vec: &Vec<&String>, s: String) -> (bool, usize) {
@@ -74,8 +122,14 @@ pub fn train(map: mapping::map<String, String>) {
         for word in key.split_whitespace() {
             let container = contains(&ckeys, word.to_string());
             if container.0 {
+                if container.1 < frequency.keys.len() {
                 frequency.values[container.1] += 1;
-            }
+                } else {
+                    frequency.keys.push(word.to_string());
+                    frequency.values.push(1);
+                }
+            };
         };
     };
+    mapping::display(frequency.reconstruct());
 }
