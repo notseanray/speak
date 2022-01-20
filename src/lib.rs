8798fbe6739d41dc3e789500225b1a5438fa01bc
Blyxyas <blyxyas@gmail.com>
2022-01-20 14:42:18 +0100

    new mechanic 'memory' added

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//region
+
 /*
 Speak crate made by Alex G. C. aka Blyxyas. Visit github.com/blyxyas/speak-rust for more information.
 
@@ -23,52 +25,50 @@ I think 4 libs will be enough.
 
 */
 
+
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(unused_variables)]
 
-// & CONSTS (Do not touch)
+//endregion
 
+// ^ CONFIG
 pub struct CONFIG {
     MULTIPLIER: u32,
-    THRESHOLD: f32
-}
-
+    THRESHOLD: f32}
 static config: CONFIG = CONFIG {
     MULTIPLIER: 17,
-    THRESHOLD: 0.1
-};
+    THRESHOLD: 0.1};
 
 
 // & TYPES (map and Deconstructed)
+// region
 pub mod mapping {
-    pub struct map<K, V> { // This is just a wrapper for the Deconstructed map.
+    pub struct map<K, V> {
+        // This is just a wrapper for the Deconstructed map.
         pub entries: Vec<(K, V)>,
     }
 
     pub(crate) struct Deconstructed<K, V> {
         pub keys: Vec<K>,
         pub values: Vec<V>,
-        pub size: usize // I know, this isn't the best way to do it, but I'm fighting with the borrow checker and the len function.
+        pub size: usize, // I know, this isn't the best way to do it, but I'm fighting with the borrow checker and the len function.
     }
 
     impl<K, V> map<K, V> {
-// Implements for maps of all types
-// ^ UX:
+        // ^ UX:
 
-            pub fn new() -> Self {
-                map {
-                    entries: Vec::new()
-                }
+        pub fn new() -> Self {
+            map {
+                entries: Vec::new(),
             }
+        }
 
-            pub fn from(entries: Vec<(K, V)>) -> map<K, V> {
-                return map {
-                    entries
-                };
-            }
+        pub fn from(entries: Vec<(K, V)>) -> map<K, V> {
+            return map { entries };
+        }
 
-// ^ Auxiliar
+        // ^ Auxiliar
 
         pub(crate) fn deconstruct(&self) -> Deconstructed<&K, &V> {
             let mut keys = Vec::new();
@@ -79,7 +79,7 @@ pub mod mapping {
                 keys.push(key);
                 values.push(value);
                 size += 1;
-            };
+            }
 
             Deconstructed { keys, values, size }
         }
@@ -92,12 +92,9 @@ pub mod mapping {
                 entries.push((&self.keys[i], &self.values[i]));
             }
 
-            return map {
-                entries
-            };
+            return map { entries };
         }
     }
-    
     pub(crate) fn translate(vec: &Vec<&String>) -> Vec<Vec<u32>> {
         // Keys:
         let mut result: Vec<Vec<u32>> = Vec::new();
@@ -107,59 +104,78 @@ pub mod mapping {
             for word in pkey.split_whitespace() {
                 for c in pkey.chars() {
                     sum += super::config.MULTIPLIER * c as u32;
-                };
+                }
                 ram.push(sum);
                 sum = 0;
-            };
+            }
             result.push(ram.clone());
             ram.clear();
-        };
+        }
         return result;
     }
 }
+
+//endregion
+
+// ^ AUXILIAR FUNCTIONS
+//region
 
 fn contains(vec: &Vec<&String>, s: String) -> (bool, usize) {
     for (i, item) in vec.iter().enumerate() {
         if item == &&s {
             return (true, i);
         };
-    };
+    }
     return (false, 0);
 }
 
-pub fn train(map: mapping::map<String, String>) -> Vec<Vec<f32>> {
-        let dec = map.deconstruct();
-        let keys = mapping::translate(&dec.keys);
-        let values = mapping::translate(&dec.values);
-
-        let mut TrainedData: Vec<Vec<f32>> = Vec::new();
-        let mut ram: Vec<f32> = Vec::new();
-        for aphrase in keys.iter() {
-            
-        }
+fn sum(vec: Vec<u32>) -> u32 {
+    let mut sum: u32 = 0;
+    for each in vec.iter() {
+        sum += each;
+    }
+    return sum;
 }
+
+//endregion
+
+pub fn train(map: mapping::map<String, String>, memory: usize) { // -> Vec<Vec<f32>>
+    let dec = map.deconstruct();
+    let keys = mapping::translate(&dec.keys);
+    let values = mapping::translate(&dec.values);
+
+    let mut TrainedData: Vec<Vec<f32>> = Vec::new();
+    let mut ram: Vec<f32> = Vec::new();
+
+    let mut guess: u32 = 0;
+    for (i, aphrase) in keys.iter().enumerate() {
+        if i - memory >= 0 {
+            // Then we guess the next word
+            // if sum(aphrase[i - memory..i].to_vec())
+        };
+    };
+}
+
+//region
 
 pub fn run(RawInput: String, map: mapping::map<String, String>, TrainedData: Vec<f32>) {
     let mut input: Vec<f32> = Vec::new();
     let mut sum: u32 = 0;
-    
     // &**********************************
     // ^ Translating the input to numbers.
 
     for (i, word) in RawInput.split_whitespace().enumerate() {
         for c in word.chars() {
             sum += config.MULTIPLIER * c as u32;
-        };
+        }
         input.push(sum as f32);
         sum = 0;
-    };
+    }
 
-    /* 
-    ^ *********************************
-    & ********************************** */
     // ^ Calculating the result
 
     let mut result: String = String::new();
-    for (i, input_word) in input.iter().enumerate() {   
-    }
+    for (i, input_word) in input.iter().enumerate() {}
 }
+
+//endregion
