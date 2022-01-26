3d29631b5b8ff7ab962d5ddcc6f7aa2fc4b64934
Blyxyas <blyxyas@gmail.com>
2022-01-26 20:40:23 +0100

    Refactoring, optimizations, and more!
    tbh I love this file rn

--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -3,61 +3,107 @@
 // * /////////////////////////////
 
 pub struct Config {
-    pub multiplier: u32,
-    pub threshold: f32,
-    pub memory: i32,
+    multiplier: u32
+    ,threshold: f32
+    ,memory:    i32
 }
 
 pub static CONFIG: Config = Config {
-    multiplier: 13,
-    threshold: 0.1,
-    memory: 1
+    multiplier: 1
+    ,threshold: 0.1
+    ,memory:    1
 };
 
 // * /////////////////////////////
-// ^ MAPS ////////////////////////
+// ^ MapS ////////////////////////
 // * /////////////////////////////
 
-pub mod mapping {
-    #[allow(non_camel_case_types)]
-    pub struct map<K, V> {
-        // This is just a wrapper for the Deconstructed map.
-        pub entries: Vec<(K, V)>,
+// A map only allows these types: String, &str, u32
+
+pub(crate) trait Allowed {}
+impl Allowed for String {}
+impl Allowed for Vec<String> {}
+
+impl Allowed for &str {}
+impl Allowed for Vec<&str> {}
+
+impl Allowed for u32 {}
+impl Allowed for Vec<u32> {}
+
+pub struct Map<K: Allowed, V: Allowed> { entries: Vec<(K, V)> }
+pub(crate) struct Deconstructed<K: Allowed, V: Allowed> { keys: Vec<K>, values: Vec<V> }
+
+// Creates a new map
+
+pub(self) fn __new__<T: Allowed>() -> Map<T, T> { return Map { entries: Vec::new() } }
+
+// Creates a new map with the given entries
+
+pub(self) fn __from__<T: Allowed>(vec1: Vec<T>, vec2: Vec<T>) -> Map<T, T> {
+    let mut entries: Vec<(T, T)> = Vec::new();
+    for (key, value) in vec1.iter().zip(vec2.iter()) { entries.push((*key, *value)); }
+    return Map { entries }; }
+
+pub(crate) fn __deconstruct__<T: Allowed>(map: Map<T, T>) -> Deconstructed<T, T> {
+    let mut keys: Vec<T> = Vec::new();
+    let mut values: Vec<T> = Vec::new();
+
+    for (key, value) in map.entries.iter() {
+        keys.push(*key);
+        values.push(*value);
     }
 
-    pub(crate) struct Deconstructed<K, V> {
-        pub keys: Vec<K>,
-        pub values: Vec<V>
-    }
-
-    impl<K, V> map<K, V> {
-        // ^ UX:
-
-        pub fn new() -> Self {
-            map {
-                entries: Vec::new(),
-            }
-        }
-
-        pub fn from(entries: Vec<(K, V)>) -> map<K, V> {
-            return map { entries };
-        }
-
-        // ^ Auxiliar
-
-        pub(crate) fn deconstruct<T>(&self) -> Deconstructed<&K, &V> {
-            let mut keys = Vec::new();
-            let mut values = Vec::new();
-
-            for (key, value) in self.entries.iter() {
-                keys.push(key);
-                values.push(value);
-            }
-
-            Deconstructed { keys, values }
-        }
-    }
+    return Deconstructed { keys, values };
 }
+
+macro_rules! impl_map {
+    ($($T: path),*) => {
+        $(
+            impl Map<$T, $T> {
+                pub fn new() -> Map<$T, $T> { return __new__::<$T>(); }
+                pub fn from(vec1: Vec<$T>, vec2: Vec<$T>) -> Map<$T, $T> { return __from__::<$T>(vec1, vec2); }
+                pub fn deconstruct(self) -> Deconstructed<$T, $T> { return __deconstruct__::<$T>(self); }
+            }
+        )*
+    };
+}
+
+
+type T = String;
+type VT = Vec<String>;
+
+type U = &'static str; 
+type VU = Vec<&'static str>;
+
+type V = u32;
+type VV = Vec<u32>;
+
+impl_map!(T, VT, U, VU, V, VV);
+
+/*
+impl<K, V> Map<K, V> {
+    // ^ UX:
+
+    pub fn new() -> Self { Map { entries: Vec::new() } }
+
+    pub fn from(entries: Vec<(K, V)>) -> Map<K, V> {
+        return Map { entries };
+    }
+
+    // ^ Auxiliar
+
+    pub(crate) fn deconstruct<T>(&self) -> Deconstructed<&K, &V> {
+        let mut keys = Vec::new();
+        let mut values = Vec::new();
+
+        for (key, value) in self.entries.iter() {
+            keys.push(key);
+            values.push(value);
+        }
+
+        Deconstructed { keys, values }
+    }
+}*/
 
 // * /////////////////////////////
 // ^ MISC. ///////////////////////
