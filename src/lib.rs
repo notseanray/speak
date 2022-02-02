dcadd841d2565768929e9b98180853112b143304
Blyxyas <blyxyas@gmail.com>
2022-02-02 20:29:52 +0100

    Minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 // Speak crate made by Alex G. C. aka Blyxyas. Visit github.com/blyxyas/speak for more information.
+
+#![allow(dead_code)]
 #[path = "lib/utils.rs"]
 pub(crate) mod utils;
+pub(crate) use utils::*;
+
 pub struct Config {
     pub multiplier: u32,
     pub threshold: f32,
-    pub memory: usize
+    pub memory: usize,
 }
 
 pub static CONFIG: Config = Config {
-    multiplier: 1
-    ,threshold: 0.3
-    ,memory: 1
+    multiplier: 1,
+    threshold: 0.3,
+    memory: 1,
 };
 
 // * /////////////////////////////
@@ -25,14 +29,28 @@ pub trait Literal {
 
 // Using the .literal() method on a string or &str returns the String.
 
-impl Literal for String { fn literal(&self) -> String { return String::from(self)} }
-impl Literal for &str { fn literal(&self) -> String { return String::from(*self); } }
+impl Literal for String {
+    fn literal(&self) -> String {
+        return String::from(self);
+    }
+}
+impl Literal for &str {
+    fn literal(&self) -> String {
+        return String::from(*self);
+    }
+}
 
-pub struct Map<T: Literal> { pub entries: Vec<(T, T)> }
+pub struct Map<T: Literal> {
+    pub entries: Vec<(T, T)>,
+}
 
 // Creates a new map
 
-pub(self) fn __new__<T: Literal>() -> Map<T> { return Map { entries: Vec::new() } }
+pub(self) fn __new__<T: Literal>() -> Map<T> {
+    return Map {
+        entries: Vec::new(),
+    };
+}
 
 // Creates a new map with the given entries
 
@@ -40,8 +58,9 @@ pub(self) fn __from__<T: Literal>(vec: Vec<(T, T)>) -> Map<String> {
     let mut entries: Vec<(String, String)> = Vec::new();
     for (key, value) in vec {
         entries.push((key.literal(), value.literal()));
-    };
-    return Map { entries }; }
+    }
+    return Map { entries };
+}
 
 // Deconstructs a map into a Deconstructed struct (two vectors of strings, keys & values)
 
@@ -52,7 +71,7 @@ pub(crate) fn deconstruct<T: Literal>(map: Map<T>) -> Deconstructed<String> {
     for (key, value) in map.entries.iter() {
         keys.push(key.literal());
         values.push(value.literal());
-    };
+    }
 
     return Deconstructed { keys, values };
 }
@@ -64,14 +83,20 @@ macro_rules! impl_map {
                 pub fn new() -> Map<$T> { return __new__::<$T>(); }
                 pub fn from(vec: Vec<($T, $T)>) -> Map<String> { return __from__::<$T>(vec); }
 
-                pub fn push(mut self, to_push: ($T, $T)) -> Map<$T> {
+                pub fn push(mut self, to_push: ($T, $T)) {
                     self.entries.push(to_push);
-                    return self;
                 }
 
-                pub fn insert(mut self, index: usize, to_insert: ($T, $T)) -> Map<$T> {
+                pub fn insert(mut self, index: usize, to_insert: ($T, $T)) {
                     self.entries.insert(index, to_insert);
-                    return self;
+                }
+
+                pub fn remove(mut self, index: usize) {
+                    self.entries.remove(index);
+                }
+
+                pub fn clear(mut self) {
+                    self.entries.clear();
                 }
             }
         )*
@@ -79,13 +104,14 @@ macro_rules! impl_map {
 }
 
 type T = String;
-type U = &'static str; 
+type U = &'static str;
 
 impl_map!(T, U);
 
 // * /////////////////////////////
 // ^ For the algorithm. //////////
 // * /////////////////////////////
+
 
 #[path = "lib/mainfunctions/train.rs"]
 pub mod train;
--- src/lib/utils.rs
+++ src/lib/utils.rs
@@ -1,8 +1,4 @@
-#![allow(dead_code, unused_variables)]
-#[path = "../lib.rs"]
-mod lib;
-use lib::*;
-
+#![allow(dead_code)]
 // * /////////////////////////////
 // ^ CONFIG //////////////////////
 // * /////////////////////////////
@@ -17,15 +13,15 @@ pub(crate) struct Translated<T> { pub dec: Deconstructed<T>, pub values: Vec<Str
 // ^ For the algorithm. //////////
 // * ////////////////////////////
 
-pub(crate) fn translate<L: lib::Literal>(vec: &Vec<L>) -> Vec<Vec<u32>> {
+pub(crate) fn translate<L: crate::Literal>(vec: &Vec<L>) -> Vec<Vec<u32>> {
     let mut ram: Vec<u32> = Vec::new();
     let mut result: Vec<Vec<u32>> = Vec::new();
     let mut sum: u32 = 0;
     for word in vec {
         let word = word.literal();
-        for (i, word) in word.split_whitespace().enumerate() {
+        for word in word.split_whitespace() {
             for c in word.chars() {
-                sum += CONFIG.multiplier * c as u32;
+                sum += crate::CONFIG.multiplier * c as u32;
             };
             ram.push(sum);
             sum = 0;
