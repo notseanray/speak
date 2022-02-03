6824b74c6a139f4860cd39b11bf2ca286792dd43
Blyxyas <blyxyas@gmail.com>
2022-02-03 19:31:30 +0100

    Minor changes

--- src/lib/mainfunctions/train.rs
+++ src/lib/mainfunctions/train.rs
@@ -4,20 +4,30 @@ pub fn train<T: Literal>(rawdata: Map<T>) {
     // ^ // Deconstructing & Translating ///
     // * ////////////////////////////////////Minor changes
     
-    let _data: Deconstructed<String> = deconstruct(rawdata);
-    let decdata: utils::Deconstructed<Vec<u32>> = Deconstructed::<Vec<u32>> {
-        keys: translate(_data.keys),
-        values: translate(_data.values)
-    };
-    
-    let mut data: (Vec<(Vec<u32>, Vec<u32>)>, usize) = (Vec::new(), 0);
-    for x in 0..decdata.values.len() {
-        data.0.push((decdata.keys[x].clone(), decdata.values[x].clone()));
+    let mut data: Vec<(Vec<u32>, Vec<u32>)> = Vec::new();
+    {
+        let _data: Deconstructed<String> = deconstruct(rawdata);
+        let decdata: utils::Deconstructed<Vec<u32>> = Deconstructed::<Vec<u32>> {
+            keys: translate(_data.keys),
+            values: translate(_data.values)
+        };
+        
+        for x in 0..decdata.values.len() {
+            data.push((decdata.keys[x].clone(), decdata.values[x].clone()));
+        }
     }
-    data.1 = decdata.values.len();
+
+    // Data.0 - Vec<(Keys, Values)>
+    // Data.1 - Length
 
     // * ////////////////////////////////////
     // ^ // Training! ///////////////////////f
     // * ////////////////////////////////////
-    println!("{:?}", data.0);
+    println!("{:?}", data);
+    let mut kl: usize = 0; // Current Key length
+    let mut kv: usize = 0; // Current Value length
+    for (key, value) in data {
+        kv = key.len();
+    }
+
 }
