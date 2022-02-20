10c5f798ba21998abdd16f1dc24f6f6fec5c3cf0
Blyxyas <blyxyas@gmail.com>
2022-02-20 01:15:13 +0100

    little part added

--- src/libs/run.rs
+++ src/libs/run.rs
@@ -1,10 +1,24 @@
+use crate::translate;
 pub(crate) fn __run__(
     input: String, // The input string
     trainingData: Vec<Vec<f32>>, // The training data (returned fro m the train function)
     threshold: f32 // The threshold (default: 0.4)
 ) -> String {
-    let mut result: String = String::from("");
-    
+    let mut result: String = String::new();
 
-    return result
+    // First, we translate the input into a vector
+    
+    let mut inputvec: Vec<f32> = Vec::new();
+    let mut sum: u32 = 0;
+    {    
+        for word in input.split_whitespace() {
+            
+            for char in word.chars() {
+                sum += char as u32;
+            };
+            inputvec.push(sum as f32);
+            sum = 0;
+        };
+    };
+    return result;
 }
