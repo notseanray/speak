57a96303d52b91e8e7f55d38791a7a0f9fef7f82
Blyxyas <blyxyas@gmail.com>
2022-05-04 17:46:09 +0200

    [`algo` + `maps`] Added `encourage_by_str`,  `disencourage`, `disencourage_by_str`

--- src/libs/mapping.rs
+++ src/libs/mapping.rs
@@ -27,7 +27,7 @@ macro_rules! easy_panic {
 	() => {};
 	($u: expr) => {
 		return;
-	}
+	};
 }
 
 pub struct Map<T> {
@@ -82,20 +82,36 @@ impl<'a, T> Map<&'a T> {
 		if idx >= klen {
 			debug_mode!("Index of key to encourage or index + how_much to encourage is out of bounds: {} + {}, length is {}", idx, how_much, klen);
 			easy_panic!(());
-		}
+		};
 		// If it underflows, it will be 0
 		if idx - how_much >= klen {
 			debug_mode!("(Key will be moved to the first rank, the program will continue) Index to search - how_much is out of bounds: {} - {}, length is {}", idx, how_much, klen);
 			move_index(self.keys.as_mut(), idx, 0);
 		};
-		self.keys.swap(idx, idx - how_much);
-		self.values.swap(idx, idx - how_much);
+		move_index(&mut self.keys, idx, idx - how_much);
+		move_index(&mut self.values, idx, idx - how_much);
 	}
 
-	pub fn encourage_by_str() {
-		unimplemented!()
+	pub fn encourage_by_str(&mut self, str_: T, how_much: usize) where T: PartialEq<T> {
+		let idx = self.keys.iter().position(|&x| *x == str_).unwrap();
+		self.encourage(idx, how_much);
 	}
 
 	pub fn disencourage(&mut self, idx: usize, how_much: usize) {
+		let klen = self.keys.len();
+		if idx >= self.keys.len() {
+			debug_mode!("Index of key to encourage or index + how_much to encourage is out of bounds: {} + {}, length is {}", idx, how_much, self.keys.len());
+			easy_panic!(());
+		};
+
+		if idx + how_much >= klen {
+			debug_mode!("(Key will be moved to the last rank, the program will continue) Index to search + how_much is out of bounds: {} + {}, length is {}", idx, how_much, klen);
+			move_index(self.keys.as_mut(), idx, klen - 1);
+		};
+	}
+
+	pub fn disencourage_by_str(&mut self, str_: T, how_much: usize) where T: PartialEq<T> {
+		let idx = self.keys.iter().position(|&x| *x == str_).unwrap();
+		self.disencourage(idx, how_much);
 	}
 }
