2bf56a8ecf1a6eeb8a8620b9a694c20a288314e6
Blyxyas <blyxyas@gmail.com>
2022-05-07 12:51:26 +0200

    Checking if Katex works in crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Speak crate made by Alex G. C. See LICENSE for more
 //! information about the copyright.
-//! $ \text{Thanks!}\ $
+//! $ E=mc^2 $
 // If you want to see the utils scroll to the last line of the file.
 
 #![doc = document_features::document_features!()]
