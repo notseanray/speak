a7b9bb09ba3336a8870b20989cde86f6078aed17
Blyxyas <blyxyas@gmail.com>
2022-05-07 18:41:47 +0200

    [`docs`] Added documentation to the `mapping` module

--- src/libs/mapping.rs
+++ src/libs/mapping.rs
@@ -30,6 +30,26 @@ macro_rules! easy_panic {
 	};
 }
 
+/// # Map<T>
+/// A map is a collection of key-value pairs, you can compare it to a hashmap or a dictionary, you can form a Map<T> with the `new()` function, or from a vector of tuples with the `from(...)` function.
+/// ## Example
+/// ```rust
+/// use mapping::Map;
+/// let mut map = Map::new();
+/// 
+/// // Let's add some values.
+/// map.push("key", "value");
+/// map.push("key2", "value2");
+/// map.push("key3", "value3");
+///
+/// Now, let's clear the map.
+/// ```
+/// 
+/// You can also make a Map<T> from a vector of tuples.
+/// ```rust
+/// use mapping::Map;
+/// let mut map = Map::from(vec![("key", "value"), ("key2", "value2"), ("key3", "value3")]);
+/// ```
 pub struct Map<T> {
 	pub(crate) keys: Vec<T>,
 	pub(crate) values: Vec<T>,
@@ -41,6 +61,12 @@ fn move_index<T>(vec: &mut Vec<T>, idx: usize, to: usize) {
 }
 
 impl<'a, T> Map<T> where T: Literal<String> {
+	/// Creates a new map.
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	/// ```
 	pub fn new() -> Self {
 		Self {
 			keys: Vec::new(),
@@ -48,24 +74,76 @@ impl<'a, T> Map<T> where T: Literal<String> {
 		}
 	}
 
+	/// Adds a new key-value pair to the map.
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	/// map.insert("key", "value");
+	/// ```
 	pub fn insert(&mut self, key: T, value: T, index: usize) {
 		self.keys.insert(index, key);
 		self.values.insert(index, value);
 	}
+
+	/// Pushes a new key-value pair to the end of the map, you can compare the `push` method with a `push` method in a `Vec`.
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	/// map.push("key", "value");
+	/// ```
 	pub fn push(&mut self, key: T, value: T) {
 		self.keys.push(key);
 		self.values.push(value);
 	}
 
+	/// Clears the map completely.
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	///
+	/// // We add some key-value pairs.
+	/// map.push("key", "value");
+	/// map.push("key2", "value2");
+	///
+	/// // Now we clear the map.
+	/// map.clear();
+	/// ```
 	pub fn clear(&mut self) {
 		self.keys.clear();
 		self.values.clear();
 	}
 
+	/// Returns an Iterator<Item = (&T, &T)> to iterate over a map behaving like an iterator of tuples.
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	/// map.push("key", "value");
+	/// map.push("key2", "value2");
+	/// for (key, value) in map.iter() {
+	/// 	println!("{}", key);
+	/// 	println!("{}", value);
+	/// }
+	/// ```
 	pub fn iter(&self) -> impl Iterator<Item = (&T, &T)> {
 		self.keys.iter().zip(self.values.iter())
 	}
 
+
+	/// Encourages some keys more than others, so you can value better *expected output* and disencourage bad *expected output*. It takes an index of the key you want to encourage and how much you want to encourage it, ***take into account that the "how much?" number must not be greater than the index of the key you want to encourage!***
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	/// map.push("key", "value");
+	/// map.push("key2", "value2");
+	/// map.push("key3", "value3");
+	/// map.push("key4", "value4");
+	/// map.encourage(2, 1);
+	/// ```
 	pub fn encourage(&mut self, idx: usize, how_much: usize) {
 		let klen = self.keys.len();
 		if idx >= klen {
@@ -81,6 +159,16 @@ impl<'a, T> Map<T> where T: Literal<String> {
 		move_index(&mut self.values, idx, idx - how_much);
 	}
 
+
+	/// Encourages a string, instead of an index, if the string doesn't exist in the map, it will panic!(), it takes a string and how much you want to encourage it, ***take into account that the "how much?" number must not be greater than the string you want to encourage!***
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let mut map = Map::new();
+	/// map.push("key", "value");
+	/// map.push("key2", "value2");
+	/// map.push("key3", "value3");
+	/// map.encourage_by_str("key2", 1);
 	pub fn encourage_by_str(&mut self, str_: T, how_much: usize)
 	where
 		T: PartialEq<T>,
@@ -89,6 +177,7 @@ impl<'a, T> Map<T> where T: Literal<String> {
 		self.encourage(idx, how_much);
 	}
 
+	/// Disencourages some keys more than others, so you can value better *expected output* and disencourage bad *expected output*. It takes an index of the key you want to disencourage and how much you want to disencourage it, ***take into account that the "how much?" number must not be greater than the index of the key you want to disencourage!***
 	pub fn disencourage(&mut self, idx: usize, how_much: usize) {
 		let klen = self.keys.len();
 		if idx >= self.keys.len() {
@@ -102,6 +191,7 @@ impl<'a, T> Map<T> where T: Literal<String> {
 		};
 	}
 
+	/// Disencourages a string, instead of an index, if the string doesn't exist in the map, it will panic!(), it takes a string and how much you want to disencourage it, ***take into account that the "how much?" number must not be greater than the string you want to disencourage!***
 	pub fn disencourage_by_str(&mut self, str_: T, how_much: usize)
 	where
 		T: PartialEq<T>,
@@ -112,6 +202,13 @@ impl<'a, T> Map<T> where T: Literal<String> {
 }
 
 impl<T> From<Vec<(T, T)>> for Map<T> where T: Literal<String> {
+	/// Creates a new map from a vector of `(T, T)`
+	/// # Examples
+	/// ```rust
+	/// use mapping::Map;
+	/// let vec = vec![("key", "value")];
+	/// let map = Map::from(vec);
+	/// ```
 	fn from(what: Vec<(T, T)>) -> Self {
 		let mut map = Self::new();
 		for (key, value) in what {
