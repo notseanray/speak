731bd6823f47f138daa4a61bbbd722717ddf00e2
Blyxyas <blyxyas@gmail.com>
2022-05-21 23:40:39 +0200

    [`doc`] Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -233,6 +233,73 @@ macro_rules! check_for_random {
 //
 
 #[inline]
+/// <h1>Learn</h1>
+///
+/// The `learn` function takes a `DynMap` and an `usize`.
+/// The `DynMap` is the data that will be learnt, being composed of key-value
+/// pairs (Expected inputs and outputs). The memory, explained at full in the
+/// [memory documentation][mem] is used to take a number of works into an
+/// imaginary *buffer* (separating the phrase into chunks.). This is useful
+/// because we don't really talk word by word, we talk *syntactic units* (for
+/// example, the phrase "What is your name?" is composed by *"What is"* & *"your
+/// name"*. Asking something, and that thing being your name.). This function
+/// is very simple, and it returns a `Vec<Vec<f32>>` meant to be fed into the
+/// [run function](run) as the *`learnt`* parameters.
+///
+/// ## How does it work?
+/// The `learn` function (being an inline wrapper of a private function) takes
+/// your input (a [*DynMap*][dynmap]) and for each key and value translates the
+/// words into numbers. Being those numbers stored in a Vec<Vec<usize>>.
+///
+/// ```text
+/// Vec<Vec<usize>>
+/// ^   ^   ^
+/// |   |   A word
+/// |   +
+/// |   Vector of words
+/// +
+/// Vector of phrases
+/// ```
+///
+/// Now, it breaks those vectors into chunks of `MEMORY` words, if the phrase is
+/// shorter than the memory, it will be just one chunk being composed of the
+/// whole phrase.
+///
+/// Now, for each chunk collection of keys, it calculates the relation between
+/// that chunk and all the chunks in the value.
+///
+/// ```text
+/// +--------------------------+
+/// |                       (Using words because they're
+/// v                       easier to understand)
+/// +----+----+-----------+    |
+/// | What is | Your name +<---+ Here we can see the calculation
+/// +----+-+--+--+---+----+    | between these two phrase. While
+/// ^ ^     ^   ^         | the value is longer than the
+/// | |     |   |         | key, this isn't a problem
+/// | +-----+   |         |    |
+/// |       |   |         | It calculates the relation
+/// +-----------+         | between the chunks by summing
+/// |       |             | the whole chunk and dividing it
+/// |       |             | by the  sum of the value.
+/// |       |             |    |
+/// +----+---+---+-----+-------+-+
+/// | Hi, my | name is | is Alex |
+/// +--------+---------+-+-------+
+///                       ^
+///                       |
+///                       +
+///                       See the Memory documentation
+///                       to see why this is repited!
+/// ```
+///
+/// After calculating the relations between these chunks, it pushes that
+/// calculation to a `Vec<f32>`, that, after all the chunks have been pushed, is
+/// cloned into a bigger vector, ready to be used in the main `Vec<Vec<f32>>`.
+///
+/// [dynmap]: ./struct.DynMap.html
+/// [mem]: ./constant.DEFAULT_MEMORY.html
+/// [run]: ./fn.run.html
 pub fn learn(map: &DynMap, memory: Option<usize>) -> Vec<Vec<f32>> {
 	match memory {
 		Some(mem) => _train(map, mem),
@@ -269,6 +336,63 @@ fn _train(map: &DynMap, MEMORY: usize) -> Vec<Vec<f32>> {
 
 #[inline]
 #[must_use = "The run function is very expensive!"]
+/// <h1>Run</h1>
+///
+/// The `run` function is the most expensive (and the most complex) function out
+/// of the main two functions (`learn` & `run`). It takes a lot of parameters
+/// (Optional a lot of them), and it returns a String. The parameters are:
+///
+/// - Input: `&str` (Obligatory)
+/// - Map: `DynMap` (Obligatory)
+/// - Learnt: `Vec<Vec<f32>>` (Obligatory)
+///
+/// - Memory: `usize` (Optional)
+/// - Threshold: `f32` (Optional)
+/// - Max Output Length: `usize` (Optional)
+/// - Range: `usize`
+///
+/// As you can see, this function takes a lot of arguments, so let's explain
+/// them:
+///
+/// ### Input
+/// The input is a `&str`, it doesn't need anything more. Just a phrase, it can
+/// be whatever you want.
+///
+/// ### Map
+/// The map is a `DynMap`, it must be the same data that the algorithm used to
+/// learn.
+///
+/// ### Learnt
+/// The learnt is a `Vec<Vec<f32>>`, you can get it from the `learn` function,
+/// **don't modify this vector**.
+///
+/// ### Memory
+/// As the [memory documentation][mem] explains, the memory is the number of
+/// words per chunk, and it's optional. Please, if you really want to understand
+/// the memory, read the documentation.
+///
+/// ### Threshold
+/// The threshold is a `f32` optional, and it's used to filter the output by
+/// differences. If your input is different from the analyzing-in-moment key,
+/// and the difference is higher than the threshold, the output will be
+/// filtered.
+///
+/// ### Max Output Length
+/// The output is always composed of various short phrases (Depending on the
+/// memory), so you can set the maximum length (in short phrases) that can
+/// contain the output.
+///
+/// If your memory is short, I recommend setting the maximum length to a high
+/// number, like 4, the other way around if your memory is long.
+///
+/// ### Range
+/// The range is a `usize` optional, and it's used to filter the key-value based
+/// on the rank in the map. If you use a high number (Take into account the
+/// length of the map), more pairs will be analyzed. I recommend setting the the
+/// range to the length of the map divided by the percentage of the map you
+/// *think* will be bad pairs (For example nonsensical input coming from users.)
+/// 
+/// **You can always learn more about these variables going to their default-value documentation.**
 pub fn run(
 	input: &str,
 	map: &DynMap,
